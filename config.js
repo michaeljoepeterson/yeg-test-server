06e584407da12f95df79c645c89a9d96f2f0f12d
Michael Peterson <michaeljoepeterson@gmail.com>
2020-03-22 11:49:26 -0600

    initial commit with db connection

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '15m';
+//exports.ADMIN_EMAILS = process.env.ADMIN_EMAILS;
+//exports.ADMIN_LOC = process.env.ADMIN_LOC;
---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const passport = require('passport');
+const {PORT, DATABASE_URL } = require('./config');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const app = express();
+app.use(jsonParser);
+app.set('trust proxy', true)
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
+
+//passport.use(localStrategy);
+//passport.use(jwtStrategy);
+
+
+function runServer( databaseUrl, port = PORT) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(databaseUrl,{ useNewUrlParser: true,useUnifiedTopology: true }, err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app.listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      });
+    });
+  }
+  
+  function closeServer() {
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
+  }
+  
+  if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+  }
+  
+  module.exports = { app, runServer, closeServer };
