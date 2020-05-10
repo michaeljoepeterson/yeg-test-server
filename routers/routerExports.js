b3540d975a29cad356655ea6e5e1a595f7038f33
Michael Peterson <michaeljoepeterson@gmail.com>
2020-05-10 12:54:56 -0600

    fix issue with import

--- routers/routerExports.js
+++ routers/routerExports.js
@@ -1,6 +1,6 @@
 const {router:userRouter} = require('./userRouter');
 const {router:studentRouter} = require('./studentRouter');
 const {router:lessonRouter} = require('./lessonRouter');
-const {router:categoryrouter} = require('./categoryrouter');
+const {router:categoryrouter} = require('./categoryRouter');
 
 module.exports = {userRouter,studentRouter,lessonRouter,categoryrouter};
