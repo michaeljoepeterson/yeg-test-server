36e2fe8956631a40624502e26972341d6831ee12
Michael Peterson <michaeljoepeterson@gmail.com>
2020-05-13 18:52:57 -0600

    fix control origin

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(function (req, res, next) {
     if(allowedOrigins.indexOf(origin) > -1){
        res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     if (req.method === 'OPTIONS') {
