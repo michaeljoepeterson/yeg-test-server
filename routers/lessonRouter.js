a7f395fbce7be4febf1f9a62db2215a8076a222e
Michael Peterson <michaeljoepeterson@gmail.com>
2020-07-28 19:56:42 -0600

    first working get by id

--- routers/lessonRouter.js
+++ routers/lessonRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {Lesson} = require('../models/lesson');
+const {User} = require('../models/user');
 const router = express.Router();
 const passport = require('passport');
 //const {checkAdminEmails,checkEmail,checkUser,checkAdminLocs} = require('../tools/toolExports');
@@ -57,6 +58,61 @@ router.get('/',(req,res) => {
             error:err
         });
     });
+});
+
+function findById(id){
+    let promise = new Promise((resolve,reject) => {
+        return Lesson.find({teacher:id}).populate('students').populate('teacher')
+
+        .then(lessons => {
+            resolve(lessons)
+        })
+        .catch(err => {
+            reject(err);
+        });
+    });
+
+    return promise;
+}
+
+function findByEmail(id){
+    let promise = new Promise((resolve,reject) => {
+        return Lesson.find({}).populate('students').populate('teacher')
+
+        .then(lessons => {
+            resolve(lessons)
+        })
+        .catch(err => {
+            reject(err);
+        });
+    });
+
+    return promise;
+}
+
+router.get('/search',(req,res) => {
+    let {id,email} = req.query;
+    if(id){
+        return findById(id)
+
+        .then(lessons => {
+            return res.json({
+                code:200,
+                lessons:lessons.map(lesson => lesson.serialize())
+            }); 
+        })
+        .catch(err => {
+            return res.json({
+                code:500,
+                message:'an error occured',
+                error:err
+            });
+        });
+    }
+    else if(email){
+
+    }
+    
 });
 
 module.exports = {router};
