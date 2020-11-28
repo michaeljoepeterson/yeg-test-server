865ed228618d92118baa8883f215ba369afc8421
michael peterson <michaeljoepeterson@gmail.com>
2020-11-28 10:06:43 -0700

    addd logging

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.set('trust proxy', true)
 app.use(function (req, res, next) {
     let origin = req.headers.origin;
     let allowedOrigins = DOMAINS.split(',');
+    console.log('origin: ',origin);
     if(allowedOrigins.indexOf(origin) > -1){
        res.setHeader('Access-Control-Allow-Origin', origin);
     }
